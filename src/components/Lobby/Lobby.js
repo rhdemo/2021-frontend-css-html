0a755ba391ebb57ddc861f4890ed89cbf41b586a
Kyle Buchanan <kylebuch8@gmail.com>
2021-01-27 07:32:43 -0500

    added waiting message to the lobby screen

--- src/components/Lobby/Lobby.js
+++ src/components/Lobby/Lobby.js
@@ -2,13 +2,19 @@ import { connect } from "react-redux";
 import Avatar from "../Avatar";
 import "./Lobby.scss";
 
-function Lobby({ player }) {
+function Lobby({ match, player }) {
+  // @TODO remove waiting message when we move away
+  // from player vs. player.
   return (
     <div className="Lobby">
       <h1>Battleship</h1>
       <h2>Lobby</h2>
       <Avatar />
       <h3>{ player.username }</h3>
+      { !match.ready ? 
+        <p>Waiting for another player to join</p>
+        : null
+      }
     </div>
   );
 }
