674f03a210b98cf4848ae730e956d74aa01a2673
Evan Shortiss <evanshortiss@gmail.com>
2021-03-29 18:39:41 +0100

    fix: variable reference in push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 printf "\n\n######## game-ui push ########\n"
 
-IMAGE_REPOSITORY=${GAME_UI_IMAGE_REPOSITORY:-quay.io/redhatdemo/2021-game-ui:latest}
+IMAGE_REPOSITORY=${IMAGE_REPOSITORY:-quay.io/redhatdemo/2021-game-ui:latest}
 
 echo "Pushing ${IMAGE_REPOSITORY}"
 docker push ${IMAGE_REPOSITORY}
