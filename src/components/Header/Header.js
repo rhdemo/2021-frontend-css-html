d277766d58775d861c450118c6b4a767db7ec7ac
Kyle Buchanan <kylebuch8@gmail.com>
2021-04-11 10:48:21 -0400

    adding ability to show points added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,58 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import "./Header.scss";
 import { ReactComponent as Badge } from "./images/badges/badge-1.svg";
 
 function Header({ theActiveBoard, player }) {
+  const [ playerPoints, setPlayerPoints ] = useState(0);
+  const [ playerPointDifferential, setPlayerPointDifferential ] = useState(0);
+  const [ playerPointDifferentialClasses, setPlayerPointDifferentialClasses ] = useState("");
   const [ pointsText, setPointsText ] = useState("points");
   const [ screenText, setScreenText ] = useState("");
+  const prevPlayerPointsRef = useRef();
+
+  prevPlayerPointsRef.current = playerPoints;
+
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+  
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+  
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
 
   useEffect(() => {
-    if (player.score === 1) {
+    if (playerPoints === 1) {
       setPointsText("point");
     }
-  }, [ player.score ]);
+
+    let timeout;
+
+    const pointDifferential = playerPoints - prevPlayerPointsRef.current;
+    setPlayerPointDifferential(pointDifferential);
+
+    if (pointDifferential > 0) {
+      setPlayerPointDifferentialClasses("show");
+
+      timeout = setTimeout(() => {
+        setPlayerPointDifferentialClasses("");
+      }, 1000);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [ playerPoints ]);
 
   useEffect(() => {
     // need bonus round
@@ -22,12 +63,23 @@ function Header({ theActiveBoard, player }) {
     }
   }, [ theActiveBoard ]);
 
+  useInterval(() => {
+    const randomPointValue = Math.floor(Math.random() * (25 - 5) + 5);
+    setPlayerPoints(playerPoints + randomPointValue);
+    prevPlayerPointsRef.current = playerPoints;
+  }, 5000);
+
   return (
     <header className="ui-header">
       <div className="ui-header-main">
         <Badge className="ui-header-main__badge" title="Badge" />
         <span className="ui-header-main__title">{ player.username }</span>
-        <span className="ui-header-main__points">{/*{ player.score } { pointsText }*/}</span>
+        <span className="ui-header-main__points">{ playerPoints } { pointsText }</span>
+        <div className="ui-header-main__points-animate">
+          <div className={ playerPointDifferentialClasses }>
+            +{ playerPointDifferential } points
+          </div>
+        </div>
       </div>
       <div className="ui-header-sub">
         <span className="ui-header-sub__text ui-screen-text">**** { screenText } ****</span>
