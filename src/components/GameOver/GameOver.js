6b76b3a4c5b7d0a9ebdc81e2d9e475bbb84459a1
Kyle Buchanan <kylebuch8@gmail.com>
2021-04-26 08:48:36 -0400

    game over state updates

--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -1,9 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { playAgain } from "./actions";
 import "./GameOver.scss";
 
 function GameOver({ player, opponent, match, playAgain, game, score }) {
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ isTop10, setIsTop10 ] = useState(false);
+  
   useEffect(() => {
     if (game.state !== "stopped") {
       return;
@@ -13,40 +16,68 @@ function GameOver({ player, opponent, match, playAgain, game, score }) {
   }, [ game ]);
 
   async function checkForWinner() {
+    setIsLoading(true);
+    
     const response = await fetch(`https://scoring-service-battleships-scoring.apps.summit-gcp.eior.p2.openshiftapps.com/scoring/${game.uuid}/ranking?max=10`)
-      .catch(err => console.log(err));
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
 
     if (!response) {
       return;
     }
     
     const data = await response.json();
+    setIsLoading(false);
+    setIsTop10(true);
   }
 
   return (
     <div className="game-over screen">
-      <h1 className="game-over__title">Game Over</h1>
-      { game.state !== "stopped" && 
-        <>
-        { match.winner === player.uuid
-          ? <>
-              <h2 className="game-over__sub-title">Congrats, { player.username }!<br />
-              You beat our artificial player - { opponent.username }!</h2>
-            </>
-          : <>
-              <h2 className="game-over__sub-title">Oh no, { player.username }!<br />
-              You lost to our artificial player -  { opponent.username }!</h2>
-            </>
-        }
-        <p>Score: { score.total }</p>
-        <button className="game-over__action" onClick={ playAgain }>Play again</button>
-        </>
+      { isLoading && 
+        <div className="loading">
+          <p>Loading...</p>
+        </div>
       }
-      { game.state === "stopped" &&
+      { !isLoading &&
         <>
-          <h2 className="game-over__sub-title">I hope you had fun. See ya next year!</h2>
-          <h3 className="game-over__sub-title">Congratulations, you've made it into the top 10!</h3>
-          <a href={ "https://docs.google.com/forms/d/e/1FAIpQLSdIxGRaccPn73DiqbD7Df_4hNm1xhiD8r1KNn-mqFBl1wEg9g/viewform?usp=pp_url&entry.651719787=" + player.uuid + "&entry.1100509786=" + player.username } className="game-over__action">Claim your prize</a>
+          { game.state !== "stopped" && 
+            <>
+            { match.winner === player.uuid
+              ? <>
+                  <h1 className="game-over__title">Congrats</h1>
+                  <h2 className="game-over__sub-title">Congratulations, { player.username }!<br />
+                  You beat our artificial player - { opponent.username }!</h2>
+                </>
+              : <>
+                  <h1 className="game-over__title">Game Over</h1>
+                  <h2 className="game-over__sub-title">Oh no, { player.username }!<br />
+                  You lost to our artificial player -  { opponent.username }!</h2>
+                </>
+            }
+            <p>Score: { score.total }</p>
+            <button className="game-over__action" onClick={ playAgain }>Play again</button>
+            </>
+          }
+          { game.state === "stopped" &&
+            <>
+              { isTop10 &&
+                <>
+                  <h1 className="game-over__title">Congrats</h1>
+                  <h2 className="game-over__sub-title">Congratulations, { player.username }!</h2>
+                  <h3>You've made into the Top 10. Click the button below to claim your prize.</h3>
+                  <a href={ "https://docs.google.com/forms/d/e/1FAIpQLSdIxGRaccPn73DiqbD7Df_4hNm1xhiD8r1KNn-mqFBl1wEg9g/viewform?usp=pp_url&entry.651719787=" + player.uuid + "&entry.1100509786=" + player.username } className="game-over__action">Claim your prize</a>
+                </>
+              }
+              { !isTop10 && 
+                <>
+                  <h1 className="game-over__title">Game Over</h1>
+                  <h2 className="game-over__sub-title">I hope you had fun. See ya next year!</h2>
+                </>
+              }           
+            </>
+          }
         </>
       }
     </div>
